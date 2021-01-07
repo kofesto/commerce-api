cb7fa99fda79dafb0f8ac43e8d1b2de927d7d4a1
koffi seglo <kofestotech@gmail.com>
2021-01-08 00:40:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,28 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+//const bodyParser = require('body-parser');
 
 dotenv.config();
 
-app.use(cors);
 app.use(express.urlencoded({extended:false}));
+//app.use(bodyParser.json({limit:"30mb",extended:true}));
+//app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
 app.use(express.json());
+
+const port = process.env.PORT || 5000;
+const DB = process.env.MONGO_DB; // database connection
+mongoose.connect(
+    DB,
+    {useNewUrlParser:true,useUnifiedTopology:true}
+    ).then(()=>app.listen(port,()=>console.log(`server running on port: ${port}`)))
+    .catch((error)=>console.log(error.message));
+
+mongoose.set('useFindAndModify',false);
+mongoose.set('useCreateIndex',true);
+mongoose.Promise = global.Promise;
+
 
 //Getting router
 const productsRoute = require('./routes/productsRoute');
@@ -18,11 +34,6 @@ const usersRoute = require('./routes/usersRoute');
 app.use('/products',productsRoute);
 app.use('/user',usersRoute);
 
-// mongoose.connect(
-//     'mongodb://localhost:27017/commerce',
-//     {useNewUrlParser:true,useUnifiedTopology:true}
-//     );
-// mongoose.set('useCreateIndex',true);
-// mongoose.Promise = global.Promise;
 
-app.listen(process.env.PORT,()=> console.log('app is running'));
+
+//app.listen(process.env.PORT,()=> console.log('app is running'));
